d15c1bb13c102d34a6dcb6c04c64e783cc5ee4c8
edu1010 <39130123+edu1010@users.noreply.github.com>
2019-07-05 03:18:40 +0200

    Modificando el codigo para conseguir titulos y imagenes

--- ExtraerContenidoWeb.py
+++ ExtraerContenidoWeb.py
@@ -1,21 +1,66 @@
-from urllib.request import urlopen
+from urllib import request
+from urllib.request import urlopen, re
 from bs4 import BeautifulSoup
 
-def conseguirTitulos():
+def conseguirTitulos(x):
     titulos = soup.find_all(True, {'class': ['info']})
+
     for i in titulos:
         # print (i.contents)
         title_tag = i.contents[3]
         print(title_tag.string)
+        texto=(title_tag.string).__str__
+        x.write(texto)
 
+
+
+def conseguirLinks():
+    for i in linkFoto:
+        # print (i.contents)
+        link = i.find('a')
+        modificarCadena(link)
+        #print(link)
+
+def modificarCadena(cadena):
+    #cadena=cadena.__str__
+    for i in cadena:
+        if(i>=4):
+            if(i+(i-1)+(i-2)+(i-3)=="ferh"):
+                x=i
+                while(x != '"'):
+                    contador=contador+1
+                    string=string+x
+                    i=i+1
+                    x=i
+                print(string)
+def crearArchivo(nombre):
+    x=open(nombre+".txt","w")
+
+def cerrarArchivo(x):
+    x.close()
 
 conexion=urlopen("https://listas.20minutos.es/lista/100-animes-mas-populares-y-mejores-de-la-historia-354787/")
 html= conexion.read()
-soup = BeautifulSoup(html)
+soup = BeautifulSoup(html,"html.parser")
+x=open("titulos.txt","w")
+conseguirTitulos(x)
+x.close()
 
-linkFoto = soup.find_all(True, {'class':['fotolista']})
+linkFoto = soup.find_all('div',{'class': ['elemento']})
 
 for i in linkFoto:
     #print (i.contents)
-    title_tag = i.contents[2]
-    print(title_tag.string)
+    link=i.find('a')
+    #modificarCadena(link)
+
+
+''''
+re=request.urlopen("https://listas.20minutos.es/lista/100-animes-mas-populares-y-mejores-de-la-historia-354787/")
+    #get("https://listas.20minutos.es/lista/100-animes-mas-populares-y-mejores-de-la-historia-354787/")
+
+
+
+filtrado = BeautifulSoup(html)
+
+for link in filtrado.find_all(True,{'class':['listas_elementos']}):
+    print(link.get('href'))'''
